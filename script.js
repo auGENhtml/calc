dc4f960d7e2817304c5550f96953ff85be876e77
auGENhtml <yuliunov33@gmail.com>
2022-06-27 00:45:16 +0300

    calc3.0

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const calc = document.querySelector('.calc');
 const result = document.querySelector('#result');
 
+
 calc.addEventListener('click', function(event) {
   if(!event.target.classList.contains('calc_btn')) return;
 
